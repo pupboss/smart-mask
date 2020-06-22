c66c3bfec9bd335b1da95fd5f6dffa05ff27d580
Li Jie <pupboss.i@gmail.com>
2020-06-22 23:53:59 +0800

    Add button

--- app/src/main/java/com/espace/smartmask/MainActivity.java
+++ app/src/main/java/com/espace/smartmask/MainActivity.java
@@ -2,13 +2,33 @@ package com.espace.smartmask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Android M Permission check
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    private void searchBluetoothDevices() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
